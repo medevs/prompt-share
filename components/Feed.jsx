d269f3ba424dc445fa27e1b73328be68180e5daf
medevs <oublihi.a@gmail.com>
2023-10-27 11:37:09 +0200

    refactor component

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,22 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-
-import PromptCard from "./PromptCard";
-
-const PromptCardList = ({ data, handleTagClick }) => {
-  return (
-    <div className='mt-16 prompt_layout'>
-      {data.map((post) => (
-        <PromptCard
-          key={post._id}
-          post={post}
-          handleTagClick={handleTagClick}
-        />
-      ))}
-    </div>
-  );
-};
+import PromptCardList from "./PromptCardList";
 
 const Feed = () => {
   const [allPosts, setAllPosts] = useState([]);
